0197b616e67dea1f2d02ea6ce3997895df7db814
Tien Le Nhat <letien@pdx.edu>
2013-07-08 01:39:29 +0000

    fix bug

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function (err, data) {
+var fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
